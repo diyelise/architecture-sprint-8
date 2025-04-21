37ad67a65b732f3e8b9c00e85b99ad6094ce536a
dmitry.eliseenko <dmitry.eliseenko@x5.ru>
2025-04-21 21:24:58 +0300

    fix cors

--- api/app.py
+++ api/app.py
@@ -7,8 +7,10 @@ from cryptography.hazmat.primitives import serialization
 from jwt import ExpiredSignatureError, DecodeError
 
 from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/reports": {"origins": "http://localhost:3000"}}) # hardcode, but it's just example(
 
 KEYCLOAK_SERVER_URL = os.getenv("KEYCLOAK_SERVER_URL", "")
 KEYCLOAK_REALM_NAME = os.getenv("KEYCLOAK_REALM_NAME", "")
@@ -23,11 +25,23 @@ def get_public_key(realm_url):
     return serialization.load_der_public_key(key_der)
 
 
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = (
+        'DNT,User-Agent,X-Requested-With,If-Modified-Since,'
+        'Cache-Control,Content-Type,Range,Authorization'
+    )
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+
 @app.route('/reports', methods=['GET'])
 def get_reports():
     try:
         token = request.headers['authorization']
         auth_token = token.split(' ')[1]
+        print(auth_token)
         public_key = get_public_key(f'{KEYCLOAK_SERVER_URL}/realms/{KEYCLOAK_REALM_NAME}/')
         userinfo = jwt.decode(auth_token, public_key, algorithms=["RS256"])
     except KeyError as e:
@@ -38,11 +52,6 @@ def get_reports():
         return Response(status=401, headers={'WWW-Authenticate': 'Bearer error="invalid_token"'})
 
     response = jsonify({'user': {'name': userinfo["name"], 'email': userinfo["email"]}})
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add("Access-Control-Allow-Headers",
-                         "DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization")
-    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
     return response
 
 
